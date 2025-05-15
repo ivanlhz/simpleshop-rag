74a2b9153db453dc291e0fbc1ef486bc9785d3a4
Iván López Hdez <ivanlhz@gmail.com>
2025-05-15 07:22:02 +0100

    feat: Added product catalog documents getter

--- tools/vector_storage.py
+++ tools/vector_storage.py
@@ -1,4 +1,6 @@
 import json
+
+from mappers.product_catalog_mapper import get_products_documents
 from mappers.profile_mapper import get_profile_documents
 
 profile_data_file_path="../data/client_profiles.json"
@@ -10,7 +12,7 @@ def get_client_profiles_documents():
     try:
         with open(profile_data_file_path, 'r', encoding='utf-8') as f:
             profiles = json.load(f)
-        return get_profile_documents(profiles, products_data_file_path)
+        return get_profile_documents(profiles, profile_data_file_path)
     except FileNotFoundError:
         print(f"Error: File not found in {profile_data_file_path}")
         return []
@@ -19,12 +21,22 @@ def get_client_profiles_documents():
         return []
 
 
-# def get_product_catalog_documents():
-#     """Get the product catalog documents"""
-#     try:
-#
-#     except FileNotFoundError:
-#         print(f"Error: File not found in {products_data_file_path}")
+def get_product_catalog_documents():
+    """Get the product catalog documents"""
+    try:
+        with open(products_data_file_path, 'r', encoding='utf-8') as f:
+            products = json.load(f)
+        return get_products_documents(products, products_data_file_path)
+    except FileNotFoundError:
+        print(f"Error: File not found at {products_data_file_path}")
+        return []
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON format in {products_data_file_path}")
+        return []
+    except KeyError as e:
+        print(f"Error: Missing key '{e}' in one of the product objects in {products_data_file_path}")
+        return []
 
 
-print(get_client_profiles_documents())
+
+print(get_product_catalog_documents())
